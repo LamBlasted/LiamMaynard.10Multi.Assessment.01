930bf766fba434b1ee23738ca93b8339d9d92cea
LamBlasted <liamamaynard2009@gmail.com>
2025-04-26 20:56:48 +1000

    Set Direction + UpdateAnim + AnimDirection

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,6 +1,9 @@
 class_name Player extends CharacterBody2D
 
+signal DirectionChanged( new_direction: Vector2 )
+
 var cardinal_direction : Vector2 = Vector2.DOWN
+const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
 var direction : Vector2 = Vector2.ZERO
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -18,3 +21,33 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+
+func SetDirection() -> bool:
+	if direction == Vector2.ZERO:
+		return false 
+	
+	var direction_id: int  = int( round( 
+		( direction + cardinal_direction * 0.1 ).angle() 
+		/ TAU * DIR_4.size() 
+		))
+	var new_dir = DIR_4[ direction_id ]
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	DirectionChanged.emit( new_dir )
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1 
+	return true 
+
+func UpdateAnimation( state : String ) -> void:
+	animation_player.play( state + "_" + AnimDirection() )
+	pass
+
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else: 
+		return "side"
