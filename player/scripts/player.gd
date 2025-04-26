68aef71ac5c888e9b4576cf58f546781c325004b
LamBlasted <liamamaynard2009@gmail.com>
2025-04-26 16:21:16 +1000

    Changing code around for RoP

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -5,18 +5,15 @@ var direction : Vector2 = Vector2.ZERO
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
-
+@onready var state_machine: PlayerStateMachine = $PlayerStateMachine
 
 func _ready() -> void:
+	state_machine.Initialize(self)
 	pass
 
 func _process(delta: float) -> void:
-	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down")  - Input.get_action_strength("up") 
-	
-	velocity = direction * mov_speed
-	
+	direction.y = Input.get_action_strength("down")  - Input.get_action_strength("up")
 	pass
 
 func _physics_process(delta: float) -> void:
