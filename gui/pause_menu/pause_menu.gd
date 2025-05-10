177b7cfb3a395913995f29c5af632d75b187fef2
LamBlasted <liamamaynard2009@gmail.com>
2025-05-10 20:24:20 +1000

    added pause menu

---
+++ gui/pause_menu/pause_menu.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+@onready var save: Button = $VBoxContainer/Save
+@onready var load: Button = $VBoxContainer/Load
+
+var is_paused : bool = false 
+
+func _ready() -> void:
+	hide_pause_menu()
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if is_paused == false:
+			show_pause_menu()
+		else:
+			hide_pause_menu()
+		get_viewport().set_input_as_handled()
+	pass
+
+func show_pause_menu() -> void: 
+	get_tree().paused = true
+	visible = true
+	is_paused = true 
+
+func hide_pause_menu() -> void:
+	get_tree().paused = false
+	visible = false
+	is_paused = false 
